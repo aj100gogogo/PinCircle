fb8de7243e982d72a70f77010d9c61dda0820868
aj100gogogo <aj100gogogo@gmail.com>
2019-03-26 23:08:24 +0800

    complete 0324 class content

---
+++ PinCircle/Assets/Scripts/BigCircle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCircle : MonoBehaviour
+{
+    public float rotateSpeed = 1f;
+
+    private void Update()
+    {
+        this.transform.Rotate(0, 0, rotateSpeed);
+    }
+}
---
+++ PinCircle/Assets/Scripts/NailGround.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NailGround : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public bool isFired = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isFired = true;
+        }
+        if (isFired == true)
+        {
+            this.transform.Translate(0, speed, 0);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isFired = false;
+        this.transform.SetParent(collision.transform);
+    }
+}
